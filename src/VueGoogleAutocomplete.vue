3bd4b3c00a0423e0bce4eed961508bd7a8f8c7de
Diego <diegodss@gmail.com>
2018-03-25 12:59:02 -0300

    Fixing administrative_level_area2 to return short_name instead of county

--- src/VueGoogleAutocomplete.vue
+++ src/VueGoogleAutocomplete.vue
@@ -110,7 +110,7 @@
                     route: 'long_name',
                     locality: 'long_name',
                     administrative_area_level_1: 'short_name',
-                    administrative_area_level_2: 'county',
+                    administrative_area_level_2: 'short_name',
                     country: 'long_name',
                     postal_code: 'short_name'
                 };
